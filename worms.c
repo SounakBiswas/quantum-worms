e856cf0805a90a379d5c48f1683b703cd18a98c5
sounak biswas <sounak.pablo@gmail.com>
2025-12-09 12:00:08 +0530

    add dual links func

--- worms.c
+++ worms.c
@@ -8,6 +8,7 @@ int get_sdl_from_sdv(int sdv, int sdvnbr);
 int get_sl_at_sdl(int sdl);
 int get_sdl_at_sl(int sl);
 int get_sl_from_sv(int sv,int svnbr);
+void add_dual_link(int sdv0, int sdv1,int *dlctr);
 //graph data structures
 int *sv_at_v;
 int *v_at_sv;
@@ -16,6 +17,7 @@ int *sdv_at_dv;
 int *l_at_sl;
 int *dl_at_sdl;
 int *l_at_dl;
+int *dl_at_l;
 int *p_at_sp;
 int *l2v[2];
 int *dl2dv[2];
@@ -25,7 +27,7 @@ int *dv_nbrctr;
 int **dv_nbr; //nbr table for dual vertices
 int **dv2dl; //site to link for dual vertices //int **dv2dl;
 int *fsp;
-int *ncr_at_l;
+int *if_hyperedge;
 int *dimer;
 void create_graph(){
     // dual graphs and links have prefix d
@@ -36,17 +38,18 @@ void create_graph(){
     int dlctr=0;
     int dvctr=0;
     int pctr=0;
-    int *svert;
     int op;
     int s1,s2,s3;
     int dsite;
     int sdv;
     int *dv_typ; //wt type
+    int v0,v1;
     //add direct graph
     for (site=0; site<nsites; site++){
         sv_at_v[vctr]=site;
         v_at_sv[site]=vctr;
         fsp[vctr]=sigma[site];
+        dv_typ[vctr]=0;
         vctr++;
     }
     int sv;
@@ -56,9 +59,13 @@ void create_graph(){
             int vnbr = v_at_sv[svnbr];
             int sl = get_sl_from_sv(sv,svnbr);
             l_at_sl[sl] = lctr;
-            l2v[0][lctr] = v_at_sv[sv];
-            l2v[1][lctr] = v_at_sv[svnbr];
-            ncr_at_l[lctr]=0;
+            v0=v_at_sv[sv];
+            v1=v_at_sv[svnbr];
+            l2v[0][lctr] = v0;
+            l2v[1][lctr] = v1;
+            dimer[lctr]= (fsp[v0]==fsp[v1]) ? 1 : 0;
+            
+            if_hyperedge[lctr]=0;
             lctr++;
         }
     }
@@ -70,27 +77,19 @@ void create_graph(){
         dvctr++;
     }
     for (sdv=0; dsite<ndsites; sdv++){
-        for(int psite=0; psite<npsites ;psite++){
-            sv = triangle[dsite][psite];
-            dv2v[psite][dvctr]= v_at_sv[sv];
-        }
-        for (int i=0; i<nsdnbrs; i++){
-            int sdvnbr=dnbr[sdv][i];
-            int sdl = get_sdl_from_sdv(sdv,sdvnbr);
-            int sl= get_sl_at_sdl(sdl);
-            int l=l_at_sl[sl];
-            if(ncr_at_l[l]==0){
-                int dv=dv_at_sdv[sdv];
-                int dvnbr=dv_at_sdv[sdvnbr];
-                dl2dv[0][dlctr] = dv;
-                dl2dv[1][dlctr] = dvnbr;
-                dv2dl[dv][dv_nbrctr[dv]++]=dlctr;
-                dv2dl[dvnbr][dv_nbrctr[dvnbr]++]=dlctr;
-                l_at_dl[dlctr]=l;
-                dlctr++;
-                ncr_at_l[l]=1;
-            }
-            else ncr_at_l[l]=-1;
+        
+        for (int i=0; i<nplaqspersite; i++){
+          sdv=sdv_at_sv[sv][i];
+          //add dual links for uptriangles
+          if(sdv%2==0){
+              int sdv0=dnbr[sdv][0];
+              add_dual_link(sdv,sdv0,&dlctr);
+              int sdv1=dnbr[sdv][1];
+              add_dual_link(sdv,sdv1,&dlctr);
+              int sdv2=dnbr[sdv][2];
+              add_dual_link(sdv,sdv2,&dlctr);
+          }
+
         }
     }
 
@@ -104,39 +103,6 @@ void create_graph(){
             dv_typ[dv]=1;
 
 
-            if(dv_set[sdv]==0){
-
-                //set direct graph
-
-                //set dual graph (**crucial**)
-                sdv_at_dv[dvctr]=sdv;
-                dv_at_sdv[sdv]=dvctr;
-                dvctr++;
-                for(int tsite=0; tsite<3; tsite++){
-                    site = triangle[op][tsite];
-                    dv2v[tsite][dvctr]= v_at_sv[site];
-                }
-                //add 3 links 
-                for (int i=0; i<nsdnbrs; i++){
-                    int sdvnbr=dnbr[sdv][i];
-                    int sdl = get_sdl_from_sdv(sdv,sdvnbr);
-                    int sl= get_sl_at_sdl(sdl);
-                    int l=l_at_sl[sl];
-                    if(ncr_at_l[l]==0){
-                        int dv=dv_at_sdv[sdv];
-                        int dvnbr=dv_at_sdv[sdvnbr];
-                        dl2dv[0][dlctr] = dv;
-                        dl2dv[1][dlctr] = dvnbr;
-                        dv2dl[dv][dvdlctr[dv]++]=dlctr;
-                        dv2dl[dvnbr][dvdlctr[dvnbr]++]=dlctr;
-                        l_at_dl[dlctr]=l;
-                        dlctr++;
-                        ncr_at_l[l]=1;
-                    }
-                    else ncr_at_l[l]=-1;
-                }
-
-            }
 
 
         }
@@ -146,33 +112,31 @@ void create_graph(){
                 sigma[sv]*=-1;
             }
 
-            // add new vertex
-            //modify_direct_graph(){
-            //}
             v_at_sv[sv]=vctr;
             fsp[vctr] = sigma[sv];
+            v0=v_at_sv[sv];
+            dv_typ[vctr]=0;
             vctr++;
             //add links for new vertex (crucial to check for hyperedges
             for(int i=0; i<6; i++){
                 int svnbr = nbr[sv][i];
                 int vnbr = v_at_sv[svnbr];
                 int sl=get_sl_from_sv(sv,svnbr);
+                v1=v_at_sv[svnbr];
                 l_at_sl[sl] = lctr;
                 if(i<3){
-                    l2v[0][lctr] = v_at_sv[sv];
-                    l2v[1][lctr] = v_at_sv[svnbr];
+                    l2v[0][lctr] = v0;
+                    l2v[1][lctr] = v1;
                 }
                 else {
-                    l2v[0][lctr] = v_at_sv[sv];
-                    l2v[1][lctr] = v_at_sv[svnbr];
+                    l2v[0][lctr] = v0;
+                    l2v[1][lctr] = v1;
                 }
-                ncr_at_l[lctr]=0;
+                dimer[lctr]= (fsp[v0]==fsp[v1]) ? 1 : 0;
+                if_hyperedge[lctr]=0;
                 lctr++;
             }
-            
-
             //dual graph vertices
-            //modify_dual_graph(sv);
             //add 6 vertices
             for (int i=0; i<nplaqspersite; i++){
               sdv=sdv_at_sv[sv][i];
@@ -189,36 +153,29 @@ void create_graph(){
               sdv=sdv_at_sv[sv][i];
               //uptriangles
               if(sdv%2==0){
-                int sdv0=dnbr[sdv][0];
-                //add_link(sdv,sdv0)
-                int sdv1=dnbr[sdv][1];
-                //add_link(sdv,sdv1)
-                int sdv2=dnbr[sdv][2];
-                //add_link(sdv,sdv2)
-
+                  int sdv0=dnbr[sdv][0];
+                  add_dual_link(sdv,sdv0,&dlctr);
+                  int sdv1=dnbr[sdv][1];
+                  add_dual_link(sdv,sdv1,&dlctr);
+                  int sdv2=dnbr[sdv][2];
+                  add_dual_link(sdv,sdv2,&dlctr);
               }
               else{
-                int sdv2=dnbr[sdv][2];
-                //add_link(sdv2,sdv)
+                  int sdv2=dnbr[sdv][2];
+                  add_dual_link(sdv2,sdv,&dlctr);
               }
-             
-
 
             }
-
-
-
-
         }
     }
 
 }
-void add_link( int sdv0, int sdv1,int *dlctr){
+void add_dual_link( int sdv0, int sdv1,int *dlctr){
     int sdl = get_sdl_from_sdv(sdv0, sdv1);
     int sl= get_sl_at_sdl(sdl);
     int l=l_at_sl[sl];
     //check if it corresponds to a hyperedge
-    if(ncr_at_l[l]==0){
+    if(if_hyperedge[l]==-1){
         int dv0=dv_at_sdv[sdv0];
         int dv1=dv_at_sdv[sdv1];
         dl2dv[0][*dlctr] = dv0;
@@ -230,10 +187,10 @@ void add_link( int sdv0, int sdv1,int *dlctr){
         dv_nbrctr[dv0]++;
         dv_nbrctr[dv1]++;
         l_at_dl[*dlctr]=l;
+        dl_at_l[l]= (*dlctr);
         (*dlctr)++;
-        ncr_at_l[l]=1;
     }
-    else ncr_at_l[l]=-1;
+    if_hyperedge[l]++;
 }
 
 
