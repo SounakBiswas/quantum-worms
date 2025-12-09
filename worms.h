96245f28bacb9e2293cab2eb43b1cf0044a3f34b
sounak biswas <sounak.pablo@gmail.com>
2025-12-09 13:49:30 +0530

    worm algorithm

---
+++ worms.h
@@ -0,0 +1,30 @@
+int get_sdl_from_sdv(int sdv, int sdvnbr);
+int get_sl_at_sdl(int sdl);
+int get_sdl_at_sl(int sl);
+int get_sl_from_sv(int sv,int svnbr);
+void add_dual_link( int sdv0, int sdv1,int *dlctr);
+extern int *sv_at_v;
+extern int *v_at_sv;
+extern int *dv_at_sdv;
+extern int *sdv_at_dv;
+extern int *l_at_sl;
+extern int *dl_at_sdl;
+extern int *l_at_dl;
+extern int *dl_at_l;
+extern int *p_at_sp;
+extern int *l2v[2];
+extern int *dv_at_dl[2];
+extern int *dv2v[3];
+extern int *dv_set;
+extern int *dv_nbrctr;
+extern int **dv_nbr; //nbr table for dual vertices
+extern int **dl_at_dv; //site to link for dual vertices //int **dl_at_dv;
+extern int *fsp;
+extern int *if_hyperedge;
+extern int *dimer;
+extern int *dv_typ;
+
+extern int vctr;
+extern int lctr;
+extern int dlctr;
+extern int dvctr;
