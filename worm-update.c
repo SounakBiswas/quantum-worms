4e4115b39a880b44af421a6ae13e4dd1a69477ee
sounak biswas <sounak.pablo@gmail.com>
2025-12-10 07:59:13 +0530

    first elementary version

--- worm-update.c
+++ worm-update.c
@@ -15,35 +15,29 @@ int get_partner(int v1,int link){
     return dv_at_dl[1][link];
 
 }
-int get_xl(int v, int nl, int dvtyp){
-  int xl;
+int get_xl(int v, int nl){
+  int xl,dl;
   // 1 dimers
+  int dn= dimer[l_at_dl[nl]];
+  xl=nl;
   if(dv_typ[v]==1){
-     if(dv_nbrctr[v]==1) 
-       return nl;
-     else{
-       xl=dl_at_dv[v][(int)(genrand_real2()*(dv_nbrctr[v]-1))];
-       if (xl==nl)
-         xl=dl_at_dv[v][dv_nbrctr[v]-1];
-     }
+    if(dn==1){
+      xl=dl_at_dv[v][(int)(genrand_real2()*(dv_nbrctr[v]-1))];
+      if (xl==nl)
+        xl=dl_at_dv[v][dv_nbrctr[v]-1];
+    }
+    else if (dn==0){
+      for( int i=0; i<dv_nbrctr[v]; i++){
+        if (dl=dl_at_dv[v][i], dimer[l_at_dl[dl]]) {
+          xl=dl;
+          break;
+        }
+      }
+    }
   }
   // 3 or one dimers allowed
   else if(dv_typ[v]==0){
-     if(dv_nbrctr[v]==1) 
-       return nl;
-     else{
-       xl=dl_at_dv[v][(int)(genrand_real2()*(dv_nbrctr[v]-1))];
-       if (xl==nl)
-         xl=dl_at_dv[v][dv_nbrctr[v]-1];
-     }
-  }
-  else if(dv_typ[v]==2){
-     if(dimer[l_at_dl[nl]]==1) 
-       return nl;
-     else{
-       //**assuming hyper edge connections have been removed
-       xl=dl_at_dv[v][(int)(genrand_real2()*(dv_nbrctr[v]))];
-     }
+      xl=dl_at_dv[v][(int)(genrand_real2()*(dv_nbrctr[v]-1))];
   }
   return xl;
 
@@ -55,6 +49,10 @@ void myopic_worm(){
   nl=-1;
   int n,x;
   int dl;
+  int occ;
+  int ln,lx;
+  int dn,dx,v;
+  nl=-1;
   if(dv_typ[start]==1){
     for( int i=0; i<dv_nbrctr[start]; i++){
       if (dl=dl_at_dv[start][i], dimer[l_at_dl[dl]]) {
@@ -67,23 +65,16 @@ void myopic_worm(){
     nl=dl_at_dv[start][(int)(genrand_real2()*dv_nbrctr[start])];
   }
 
-  int v=get_partner(start,nl);
-  n=start;
-
-  //do the worm pivot
-  if(dv_typ[v]==1){
-    for( int i=0; i<dv_nbrctr[v]; i++){
-      if (dimer[dl_at_dv[start][i]]) {
-        nl=dl_at_dv[start][i];
-        break;
-      }
-    }
+  v=get_partner(start,nl);
+  while(v!=start){
+     xl = get_xl(v, nl);
+     nl=xl;
+     v=get_partner(v,xl);
+     ln=l_at_dl[nl];
+     lx=l_at_dl[xl];
+     dimer[ln]*=-1;
+     dimer[lx]*=-1;
+     //**check winding
   }
-
-
-
-
-  
-
 
 }
