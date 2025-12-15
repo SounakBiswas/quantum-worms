e776ce3e02ba2196c8909c8f7ff2894c74f24218
sounak biswas <sounak.pablo@gmail.com>
2025-12-15 12:55:43 +0530

    test compile script

--- compile.sh
+++ compile.sh
@@ -1,2 +1,2 @@
     job=L3j1.0000t1.0000h1.0000
-gcc -O -o $job.out -fcommon link_list.c set_constant_values.c make_lattice.c do_initialisation.c mt19937ar.c nrutil.c do_updates.c bin.c measure.c save.c main.c autocorr.c -lm -g
+gcc -O -o $job.out -fcommon link_list.c set_constant_values.c make_lattice.c do_initialisation.c mt19937ar.c  do_updates.c bin.c measure.c main.c autocorr.c utils_dual_graph.c construct_dual.c -lm -g
