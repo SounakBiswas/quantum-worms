082ec8d88bdef3168a8e0f4d5c477ba7ba6924ab
sounak biswas <sounak.pablo@gmail.com>
2025-12-15 12:58:24 +0530

    remove older unnecessary vars

--- dual_graphs.h
+++ dual_graphs.h
@@ -2,7 +2,6 @@
 #include<stdio.h>
 int get_sdl_from_sdv(int sdv, int sdvnbr);
 int get_sl_at_sdl(int sdl);
-int get_sdl_at_sl(int sl);
 int get_sl_from_sv(int sv,int svnbr);
 void add_dual_link( int sdv0, int sdv1);
 extern int *sv_at_v;
@@ -12,30 +11,10 @@ extern int *sdv_at_dv;
 extern int *dl_at_sdl;
 extern int *l_at_dl;
 extern int *dl_at_l;
-extern int *p_at_sp;
-extern int *v_at_l[2];
-extern int *dv_at_dl[2];
-extern int *dv2v[3];
-extern int *dv_set;
-extern int *dv_nbrctr;
-extern int *v_nbrctr;
 extern int **dv_nbr; //nbr table for dual vertices
 //extern int *dl_at_dv[3]; //site to link for dual vertices //int **dl_at_dv;
-extern int **dl_at_dv; //site to link for dual vertices //int **dl_at_dv;
-extern int **l_at_v;
-extern int *fsp;
-extern int *if_hyperedge;
-extern int *dimer;
-extern int *backup_dimer;
-extern int *dv_typ;
 
-extern int vctr;
-extern int lctr;
-extern int dlctr;
-extern int dvctr;
 
-extern int *wx_mark;
-extern int *wy_mark;
 
 typedef struct link link;
 typedef struct dlink dlink;
