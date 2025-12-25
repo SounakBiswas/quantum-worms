42b819ee010b7e34952c7517aaee7d54ae4c26fb
sounak biswas <sounak.pablo@gmail.com>
2025-12-25 09:28:31 +0530

    new compile scr

--- compile.sh
+++ compile.sh
@@ -1,2 +1,3 @@
     job=L3j1.0000t1.0000h1.0000
-gcc -O -o $job.out -fcommon link_list.c set_constant_values.c make_lattice.c do_initialisation.c mt19937ar.c  do_updates.c bin.c measure.c main.c autocorr.c utils_dual_graph.c construct_dual.c -lm -g
+#gcc -O -o $job.out -fcommon link_list.c set_constant_values.c make_lattice.c do_initialisation.c mt19937ar.c  do_updates.c bin.c measure.c main.c autocorr.c utils_dual_graph.c construct_dual.c -lm -g
+gcc -O -o $job.out -fcommon link_list.c set_constant_values.c make_lattice.c do_initialisation.c mt19937ar.c  do_updates.c bin_min.c measure_min.c main.c autocorr.c fat_utils.c construct_fat.c -lm -g
